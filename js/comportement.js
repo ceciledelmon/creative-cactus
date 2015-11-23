ed66ee3baf729ed4b9a2ebe45b801d33f82ca03c
Cécile Delmon <cecile.delmon@gmail.com>
2015-11-23 17:13:48 +0100

    Portfolio Responsive

--- js/comportement.js
+++ js/comportement.js
@@ -50,9 +50,50 @@ $(document).ready(function(){
         mn.removeClass(mnfix);
       }
     });
+    
+    
+    function triangleRezise(){
+   $('.triangle').each(function(){
+      var parentWidth = $(this).parent().width();
+      var parentHeight = $(this).parent().height();
+      var widthVariable = $(this).attr('w');
+      if(!widthVariable){ var widthVariable = $(this).attr('h');}
+      triangleWidth = parseInt(widthVariable) / 100 * parentWidth;
+      var heightVariable = $(this).attr('h');
+      if(!heightVariable){ var heightVariable = $(this).attr('w');}
+      triangleHeight = parseInt(heightVariable) / 100 * parentWidth;
+      
+      if($(this).hasClass('up')) {
+        triangleWidth = triangleWidth / 2;
+        triangleHeight = "" + triangleHeight + "px";
+        triangleWidth = "" + triangleWidth + "px";
+        var triangle = "0px " + triangleWidth + " " + triangleHeight + " " + triangleWidth;
+        $(this).css("border-width", triangle);
+      }
+     else if($(this).hasClass('down')) {
+        triangleWidth = triangleWidth / 2;
+        triangleHeight = "" + triangleHeight + "px";
+        triangleWidth = "" + triangleWidth + "px";
+        var triangle = triangleHeight + " " + triangleWidth + " " + "0px " + triangleWidth;
+        $(this).css("border-width", triangle);
+     }
+     else{};
+    });
+};
+    
+
+        triangleRezise();
+        $(window).resize(function() {
+          triangleRezise()
+        });
+    
+    
+    
+    
+    
 })
 
-
+    
 
     
     
